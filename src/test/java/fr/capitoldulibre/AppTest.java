0ab22df85eeadbd3a86959f4c8c82de4381969cd
Michaël Pailloncy <michael.pailloncy@gmail.com>
2016-11-19 23:03:50 +0100

    Add unit tests

---
+++ src/test/java/fr/capitoldulibre/AppTest.java
@@ -0,0 +1,15 @@
+package fr.capitoldulibre;
+
+import org.junit.Test;
+
+public class AppTest extends BaseTest  {
+
+
+    @Test
+    public void index() throws Exception {
+        server.get("/")
+                .expect(200)
+                .header("Content-Type", "text/html;charset=UTF-8");
+    }
+
+}
